51265ae29d91075af778131a20a07b648aec82b4
iconenemy <iconenemy@gmail.com>
2023-06-07 23:54:06 +0300

    fix

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const token = process.env.API_ID;
 const bot = new TelegramBot(token, { polling: true });
 
 bot.onText(/\/start/, (msg) => {
-  cron.schedule("0 50 23 7 * *", () =>
+  cron.schedule("0 58 23 7 * *", () =>
     bot.sendMessage(
       msg.chat.id,
       "<b><i> Вже завтра знімається щомісячна підписка на YouTube Premium.</i></b> \n\n <i>Прошу кожного сплатити дану операцію за реквізитами:</i> \n\n Monobank: 5375411423428171 \n Тариф: 30 грн",
